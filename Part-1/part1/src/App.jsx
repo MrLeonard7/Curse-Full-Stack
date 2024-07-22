f579ffe49b59214fdc5e3a855fa894ecc5dbcd33
David B. <89559408+MrLeonard7@users.noreply.github.com>
2024-07-22 16:22:16 -0500

    Avance

--- Part-1/part1/src/App.jsx
+++ Part-1/part1/src/App.jsx
@@ -1,18 +1,38 @@
 import { useState } from 'react'
 
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+const Button =(props) => {
+  console.log(props)
+  const {handleClick, text} = props
+  return(  
+  <button onClick={handleClick} > {text} </button>
+  )
+}
+
 const App = () => {
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
-
   const [allClicks, setAll] = useState([])
-
 
   const handleLeftClick = () => {
     setAll(allClicks.concat('L'))
     setLeft(left + 1)
   }
-
-
+  
   const handleRightClick = () => {
     setAll(allClicks.concat('R'))
     setRight(right + 1)
@@ -21,12 +41,15 @@ const App = () => {
   return (
     <div>
       {left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>right</button>
+      <Button handleClick={handleLeftClick} text={'Left'} />
+      <Button handleClick={handleRightClick} text={'Right'} />
       {right}
-
       <p>{allClicks.join(' ')}</p>
+
+      <History allClicks={allClicks} />
+
     </div>
   )
 }
+
 export default App
