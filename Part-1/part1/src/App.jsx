c18f69ef8f9baaed151eb5612e21bd5bc279f41e
David B. <89559408+MrLeonard7@users.noreply.github.com>
2024-07-26 23:00:34 -0500

    Ejercicio 2.6

--- Part-1/part1/src/App.jsx
+++ Part-1/part1/src/App.jsx
@@ -1,32 +1,28 @@
 import { useState } from 'react'
 
-const Display = props => <div> {props.value} </div>
-
-const Button = (props) => (
-  <button onClick={props.handleClick} >
-    {props.text}
-  </button>
-)
-
-const App =() => {
-  const [value, setValue] = useState(10)
-
-  const setToValue = (newValue) => {
-    console.log('value now', newValue)
-    setValue(newValue)
-  }
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
 
   return (
     <div>
-      <Display value={value} />
-
-      <Button handleClick={() => setToValue(1000)} text={'thousand'} />
-      <Button handleClick={() => setToValue(0)} text={'reset'} />      
-      <Button handleClick={() => setToValue(value + 1)} text={'increment'} />
-
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+      debug: {newName}
+      </div>
     </div>
   )
-
 }
 
 export default App
