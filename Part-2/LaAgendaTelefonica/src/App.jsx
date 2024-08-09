ce489615a99f24f661a0cfce51345fa9e57fdf1f
David B. <89559408+MrLeonard7@users.noreply.github.com>
2024-08-08 21:13:41 -0500

    Ejercicio 2.9

--- Part-2/LaAgendaTelefonica/src/App.jsx
+++ Part-2/LaAgendaTelefonica/src/App.jsx
@@ -7,11 +7,14 @@ const Persons = (props) => <p> {props.name} {props.number} </p>
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-11234567'}
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])  
   const [newName, setNewName] = useState('')
-
   const [newNumber, setNewNumber] = useState('')
+  const [search, setShearch] = useState('')
 
   const handleNameChange = (event) => {
     console.log(event.target.value)
@@ -53,10 +56,29 @@ const App = () => {
   } 
   //console.log(persons);
 
+  const searcher = (e) => {
+    setShearch(e.target.value)
+    console.log(e.target.value)
+    
+  }
+
+  let results = []
+  if (!search) {
+    results = persons
+  }else{
+    results = persons.filter((person) => (
+      person.name.toLowerCase().includes(search.toLowerCase())
+    ))
+  }
+
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter show with: <input type='text' onChange={searcher} placeholder='Filter' value={search}  />
+      </div>
+      <h2>Add a new</h2>
       <form onSubmit={addPerson} >
         <div>
           name: <input value={newName} onChange={handleNameChange} required/>
@@ -70,7 +92,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map(person => <Persons key={person.name} name={person.name} number={person.number} />)}
+        {results.map(person => <Persons key={person.name} name={person.name} number={person.number} />)}
       </div>
     </div>
   )
